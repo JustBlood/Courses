bc6932155232bc169cf6e8532da3b4c287b89d4e
JustBlood <msg.msgq@mail.ru>
2024-02-25 20:38:00 +0300

    fix[security-service]: add import

--- security-service/src/main/java/ru/just/securityservice/service/SecurityService.java
+++ security-service/src/main/java/ru/just/securityservice/service/SecurityService.java
@@ -9,6 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
