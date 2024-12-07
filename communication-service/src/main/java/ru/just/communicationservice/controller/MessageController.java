bf6f1f7db0ba6e07fc746517694caf8dc4bfc760
Timiry <irina.v.tim@gmail.com>
2024-12-07 18:23:36 +0300

    feature: add getMessageFromChat endpoint

---
+++ communication-service/src/main/java/ru/just/communicationservice/controller/MessageController.java
@@ -0,0 +1,23 @@
+package ru.just.communicationservice.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.just.communicationservice.dto.PageDto;
+import ru.just.communicationservice.model.Message;
+import ru.just.communicationservice.service.MessageService;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/messages")
+@RequiredArgsConstructor
+public class MessageController {
+    private final MessageService messageService;
+
+    @GetMapping("/{chatId}")
+    public ResponseEntity<Page<Message>> getMessagesForChat(@PathVariable UUID chatId, @RequestParam("page") int page, @RequestParam("size") int size) {
+        return ResponseEntity.ok(messageService.loadMessages(chatId, new PageDto(page, size)));
+    }
+}
--- communication-service/src/main/java/ru/just/communicationservice/dto/PageDto.java
+++ communication-service/src/main/java/ru/just/communicationservice/dto/PageDto.java
@@ -1,8 +1,10 @@
 package ru.just.communicationservice.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class PageDto {
     int page = 0;
     int size = 20;
